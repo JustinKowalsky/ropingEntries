0c6566511be195fb464571520743c998eb8ed545
Justin-PC <justinkowalsky@live.com>
2018-03-01 21:55:40 -0500

    first commit

---
+++ ropingEntries/ropingEntries/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace ropingEntries
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.entryName = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.addEntry = new System.Windows.Forms.Button();
+            this.randomEntires = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pickPartner = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.csvExport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // entryName
+            // 
+            this.entryName.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.entryName.Location = new System.Drawing.Point(146, 27);
+            this.entryName.Name = "entryName";
+            this.entryName.Size = new System.Drawing.Size(221, 20);
+            this.entryName.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(128, 13);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Entrant Name:";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // addEntry
+            // 
+            this.addEntry.Location = new System.Drawing.Point(292, 145);
+            this.addEntry.Name = "addEntry";
+            this.addEntry.Size = new System.Drawing.Size(75, 23);
+            this.addEntry.TabIndex = 2;
+            this.addEntry.Text = "Add Entry";
+            this.addEntry.UseVisualStyleBackColor = true;
+            this.addEntry.Click += new System.EventHandler(this.addEntry_Click);
+            // 
+            // randomEntires
+            // 
+            this.randomEntires.Location = new System.Drawing.Point(146, 221);
+            this.randomEntires.Name = "randomEntires";
+            this.randomEntires.Size = new System.Drawing.Size(198, 64);
+            this.randomEntires.TabIndex = 3;
+            this.randomEntires.Text = "Randomize Entries";
+            this.randomEntires.UseVisualStyleBackColor = true;
+            this.randomEntires.Click += new System.EventHandler(this.randomEntries_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(246, 62);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "Please choose a role";
+            // 
+            // pickPartner
+            // 
+            this.pickPartner.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.pickPartner.Location = new System.Drawing.Point(146, 98);
+            this.pickPartner.Name = "pickPartner";
+            this.pickPartner.Size = new System.Drawing.Size(221, 20);
+            this.pickPartner.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(12, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(128, 13);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "Pick 1 Partner";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // csvExport
+            // 
+            this.csvExport.Location = new System.Drawing.Point(49, 427);
+            this.csvExport.Name = "csvExport";
+            this.csvExport.Size = new System.Drawing.Size(133, 33);
+            this.csvExport.TabIndex = 7;
+            this.csvExport.Text = "Export to CSV";
+            this.csvExport.UseVisualStyleBackColor = true;
+            this.csvExport.Click += new System.EventHandler(this.csvExport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 505);
+            this.Controls.Add(this.csvExport);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pickPartner);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.randomEntires);
+            this.Controls.Add(this.addEntry);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.entryName);
+            this.Name = "Form1";
+            this.Text = "Kinsman Quarter Horses";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox entryName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button addEntry;
+        private System.Windows.Forms.Button randomEntires;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox pickPartner;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button csvExport;
+    }
+}
+
---
+++ ropingEntries/ropingEntries/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+namespace ropingEntries
+{
+    public partial class Form1 : Form
+    {
+        List<string> headerEntries = new List<string>();
+        List<string> heelerEntries = new List<string>();
+        List<string> heelerEntriesTwo = new List<string>();
+        static Random rnd = new Random();
+        int headerCount = 0;
+        int heelerCount = 0;
+        
+        public Form1()
+        {
+            headerEntries.Add("A");
+            headerEntries.Add("B");
+            headerEntries.Add("C");
+            headerEntries.Add("D");
+            headerEntries.Add("E");
+            headerEntries.Add("F");
+            heelerEntries.Add("G");
+            heelerEntries.Add("H");
+            heelerEntries.Add("I");
+            heelerEntries.Add("J");
+            heelerEntries.Add("K");
+            heelerEntries.Add("L");
+            heelerEntriesTwo.Add("G");
+            heelerEntriesTwo.Add("H");
+            heelerEntriesTwo.Add("I");
+            heelerEntriesTwo.Add("J");
+            heelerEntriesTwo.Add("K");
+            heelerEntriesTwo.Add("L");
+
+            InitializeComponent();
+            comboBox1.Items.Add("Header");
+            comboBox1.Items.Add("Heeler");
+            using (StreamWriter writeRopers = new StreamWriter("headers.txt"))
+            {
+                writeRopers.Write("");
+            }
+            using (StreamWriter writeRopers = new StreamWriter("heelers.txt"))
+            {
+                writeRopers.Write("");
+            }
+            using (StreamWriter writeRopers = new StreamWriter("heelers2.txt"))
+            {
+                writeRopers.Write("");
+            }
+            using (StreamWriter writeRopers = new StreamWriter("myTeams.txt"))
+            {
+                writeRopers.Write("Choose Partner Round");
+                writeRopers.Write("");
+            }
+            /*
+            using (StreamWriter writeRopers = new StreamWriter("teams.txt"))
+            {
+                writeRopers.Write("");
+            }
+            */
+        }
+
+        private void addEntry_Click(object sender, EventArgs e)
+        {       
+            if (entryName.Text == "")
+            {
+                MessageBox.Show("Please enter a name for the entrant");
+                return;
+            }
+            if (comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please choose a role");
+                return;
+            }
+            if (pickPartner.Text == "")
+            {
+                MessageBox.Show("Please pick a partner to rope with");
+                return;
+            }         
+            else
+            {
+                if(comboBox1.SelectedIndex == 0)
+                {
+                    headerEntries.Add(entryName.Text);                   
+                    using (StreamWriter writeRopers = File.AppendText("headers.txt"))
+                    {
+                        writeRopers.WriteLine(entryName.Text);
+                    }
+                    using (StreamWriter writeRopers = File.AppendText("teams.txt"))
+                    {
+                        writeRopers.WriteLine(entryName.Text + " & " + pickPartner.Text);
+                    }
+                    using (StreamWriter writeRopers = File.AppendText("myTeams.txt"))
+                    {
+                        writeRopers.WriteLine(entryName.Text + " & " + pickPartner.Text);
+                    }
+                    Debug.WriteLine(entryName.Text + " has been added as a header");
+                    MessageBox.Show(entryName.Text + " has been added as a header, with " + pickPartner.Text + " as their heeler");
+                    headerCount = headerCount + 1;
+                    entryName.Clear();
+                    pickPartner.Clear();
+                    comboBox1.SelectedIndex = -1;
+                }
+                if (comboBox1.SelectedIndex == 1)
+                {
+                    heelerEntries.Add(entryName.Text);
+                    heelerEntriesTwo.Add(entryName.Text);
+                    using (StreamWriter writeRopers = File.AppendText("heelers.txt"))
+                    {
+                        writeRopers.WriteLine(entryName.Text);
+                    }
+                    using (StreamWriter writeRopers = File.AppendText("teams.txt"))
+                    {
+                        writeRopers.WriteLine(pickPartner.Text + " & " + entryName.Text);
+                    }
+                    using (StreamWriter writeRopers = File.AppendText("myTeams.txt"))
+                    {
+                        writeRopers.WriteLine(pickPartner.Text + " & " + entryName.Text);
+                    }
+                    Debug.WriteLine(entryName.Text + " has been added as a heeler");
+                    MessageBox.Show(entryName.Text + " has been added as a heeler, with " + pickPartner.Text + " as their header");
+                    heelerCount = heelerCount + 1;
+                    entryName.Clear();
+                    pickPartner.Clear();
+                    comboBox1.SelectedIndex = -1;
+                }
+            }
+        }
+
+        private void randomEntries_Click(object sender, EventArgs e)
+        {
+            /*
+            if(headerCount == 0)
+            {
+                MessageBox.Show("Please enter at least 1 header");
+            }
+            if (heelerCount == 0)
+            {
+                MessageBox.Show("Please enter at least 1 heeler");
+            }
+            */
+            //Debug.WriteLine("Headers: ");
+           
+            foreach (String i in headerEntries)
+            {
+                int spotOne = rnd.Next(heelerEntries.Count);
+                var partnerOne = heelerEntries[spotOne];
+                using (StreamWriter writeRopers = File.AppendText("myTeams.txt"))
+                {
+                    writeRopers.WriteLine(" ");
+                    writeRopers.WriteLine("Draw Round One");
+                    writeRopers.WriteLine(i + " & " + partnerOne);
+                }               
+                String firstRound = (i + "&" + partnerOne);
+                heelerEntries.RemoveAt(spotOne);
+
+                int spotTwo = rnd.Next(heelerEntries.Count);
+                var partnerTwo = heelerEntriesTwo[spotTwo];
+                String secondRound = (i + " & " + partnerTwo);
+                while (firstRound == secondRound)
+                {
+                    spotTwo = rnd.Next(heelerEntries.Count);
+                    partnerTwo = heelerEntriesTwo[spotTwo];
+                    secondRound = (i + " & " + partnerTwo);
+                }
+                using (StreamWriter writeRopers = File.AppendText("myTeams.txt"))
+                {
+                    
+                    writeRopers.WriteLine("Draw Round Two");
+                    writeRopers.WriteLine(i + " & " + partnerTwo);
+                    writeRopers.WriteLine(" ");
+                }
+                heelerEntriesTwo.RemoveAt(spotTwo);
+            }
+        }
+
+        private void csvExport_Click(object sender, EventArgs e)
+        {
+            string[] teams = File.ReadAllLines("teams.txt");
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendLine("Header, Heeler, Run 1, Run 2, Run 3, Total");
+            foreach (string team in teams)
+            {
+                var temp = team.Split('&');
+                builder.AppendLine(string.Format("{0}, {1}", temp[0], temp[1]));
+            }
+
+            File.WriteAllText("myTeams.csv", builder.ToString());
+        }
+    }
+}
